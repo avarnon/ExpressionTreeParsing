f37ff4b49fd712c85864027545f995232d069cc7
Andrew Varnon <avarnon@rightpoint.com>
2020-01-08 15:37:13 -0500

    Add project files.

---
+++ ExpressionTreeParsing.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionTreeParsing.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
